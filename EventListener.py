070009af0bf545c4848a6b6f54dafc8d34281df9
Callum Bartlett <coolum444@hotmail.co.uk>
2016-11-30 02:28:12 +0000

    :-1: Very temporary command prefix set
    This is probably the worst possible way I could have done it, but the
    API currently doesn't seem to allow for the "ready" listener, and it
    also doesn't seem to have documentation for the config.yaml file
    
    This will be changed as soon as an update with a cleaner change is
    pushed.

--- EventListener.py
+++ EventListener.py
@@ -1,14 +1,24 @@
 from disco.bot import Bot, Plugin
 from holster.emitter import Priority
+from disco.util.config import Config
 
 
 class EventListener(Plugin):
 
     # Is to be run when a channel is updated
     @Plugin.listen('ChannelUpdate', priority=Priority.BEFORE)
-    def on_channel_create(self, event):
+    def on_channel_update(self, event):
         preChannel = self.state.channels.get(event.channel.id)
         aftChannel = event.channel
         transition = [preChannel, aftChannel]
 
         # Currently does nothing
+
+    # Event listener - on message
+    # Currently sets the command prefix since config isn't documented
+    # This is a very very very bad way of doing things
+    @Plugin.listen('MessageCreate')
+    def on_message(self, event):
+        # Sets the command prefix to ';'
+        self.bot.config.commands_prefix = ';'
+        self.bot.config.commands_require_mention = False
