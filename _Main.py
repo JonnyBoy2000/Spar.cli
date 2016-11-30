31e069215a2755d94fbbd4ef6e38d663dc2466b2
Win_PCLaptop Callum Bartlett <coolum444@hotmail.co.uk>
2016-11-30 14:24:28 +0000

    :sparkles: Extentions now Loading
    Command parser also updated to be able to handle later upgrades if
    necessary

--- _Main.py
+++ _Main.py
@@ -3,7 +3,11 @@ from discord.ext import commands
 from sys import argv
 
 
-sparcli = command.Bot(command_prefix=';', description='fuck')
+initialExtentions = ['Cogs.Admin',
+                     'Cogs.Misc']
+
+
+sparcli = commands.Bot(command_prefix=';', description='fuck')
 
 
 @sparcli.command(pass_context=True)
@@ -14,4 +18,32 @@ async def ev(ctx, *, content: str):
     await sparcli.say(eval(content))
 
 
-sparcli.run(argv[0])
+@sparcli.event
+async def on_ready():
+    print('-----')
+    print('User :: {}'.format(sparcli.user))
+    print('ID :: {}'.format(sparcli.user.id))
+    print('-----')
+
+    # Load the extentions
+    for extension in initialExtentions:
+        # This is necessary because I'm bad at code
+        try:
+            sparcli.load_extension(extension)
+
+        # Print out any errors
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+
+args = {'--token': None}
+del argv[0]  # Delete the name from the cli
+
+
+# Format the args into a dictionary
+for i in range(0, len(argv), 2):
+    args[argv[i]] = argv[i+1]
+
+
+sparcli.run(args['--token'])
