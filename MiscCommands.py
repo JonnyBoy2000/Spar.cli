bec71d55b08828399e1b28435a0c83a8fc19803f
Callum Bartlett <coolum444@hotmail.co.uk>
2016-11-30 00:19:38 +0000

    :sparkles: Added git command
    The git command gives out the bot's github repo link.
    Other commands are now commented
    
    Top priorities for command additions ::
    * Ban
    * Config (Server configs - needed for event listeners)
    * CC (Custom commands)
    * Eval
    * Pun (Use beautifulsoup4)
    * Cat (Web scraping)
    * Lenny (Simple reply feature)
    * Game (Change bot presence(
    * Restart/Kill (Can use sys)
    * Info (Give information on a tagged user)
    * Clean/Purge (Delete messages through a channel)

--- MiscCommands.py
+++ MiscCommands.py
@@ -2,12 +2,20 @@ from disco.bot import Bot, Plugin
 
 class MiscCommands(Plugin):
 
-
     @Plugin.command('echo', '<content:str...>')
     def on_echo_command(self, event, content):
+        '''echo <Content>
+        Repeats back what the user passes as an argument.'''
         event.msg.reply(content)
 
     @Plugin.command('invite')
     def on_invite_command(self, event):
+        '''invite
+        Gives an invite link to add the bot to a server'''
         event.msg.reply('https://discordapp.com/oauth2/authorize?client_id=252880131540910080&scope=bot&permissions=0')
 
+    @Plugin.command('git')
+    def on_git_command(self, event):
+        '''git
+        Gives a link to the bot's git repo'''
+        event.msg.reply('https://github.com/4Kaylum/Spar.cli/')
