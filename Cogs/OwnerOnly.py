437d6cbd408d851b4807be36096191c13ff1fc38
Callum Bartlett <coolum444@hotmail.co.uk>
2016-12-01 01:01:30 +0000

    :sparkles: Added av command
    This command allows anyone with the owner flag to change the avatar of
    the bot, either through giving a link, or uploading an image.

--- Cogs/OwnerOnly.py
+++ Cogs/OwnerOnly.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from discord import Game
+from requests import get
 from sys import path
 path.append('../')  # Move path so you can get the Utils folder
 from Utils.Discord import getPermissions
@@ -16,7 +17,8 @@ class OwnerOnly:
         Usage :: game <Content>'''
 
         # Check if the owner is calling the command
-        permReturn = getPermissions(ctx.message.channel, 'is_owner', ctx.message.author)
+        permReturn = getPermissions(
+            ctx.message.channel, 'is_owner', ctx.message.author)
         if type(permReturn) == str:
             await self.sparcli.say(permReturn)
             return
@@ -25,16 +27,45 @@ class OwnerOnly:
         await self.sparcli.change_presence(game=Game(name=game))
         await self.sparcli.say('Game changed to **{}**.'.format(game))
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, hidden=True)
     async def ev(self, ctx, *, content: str):
         # Check if the owner is calling the command
-        permReturn = getPermissions(ctx.message.channel, 'is_owner', ctx.message.author)
+        permReturn = getPermissions(
+            ctx.message.channel, 'is_owner', ctx.message.author)
         if type(permReturn) == str:
             await self.sparcli.say(permReturn)
             return
 
         # Eval and print the answer
         await self.sparcli.say(eval(content))
+
+    @commands.command(pass_context=True, hidden=True)
+    async def av(self, ctx, *, avatarUrl: str=None):
+        '''Changes the bot's avatar to a set URL
+        Usage :: av <ImageLink>
+              :: av <ImageUpload>'''
+
+        # Check if the owner is calling the command
+        permReturn = getPermissions(ctx.message.channel, 'is_owner', ctx.message.author)
+        if type(permReturn) == str:
+            await self.sparcli.say(permReturn)
+            return
+
+        # Checks for the URL - either passed as argument or embed
+        try:
+            if avatarUrl == None:
+                avatarUrl = ctx.message.attachments[0]['url']
+        except IndexError:
+            # If you get to this point, there's no image
+            await self.sparcli.say('You need to pass an image or url to set the avatar to.')
+            return
+
+        # Load up the image
+        imageData = get(avatarUrl).content 
+
+        # Set profile picture
+        await self.sparcli.edit_profile(avatar=imageData)
+        await self.sparcli.say("Profile picture successfully changed.")
 
 
 def setup(bot):
