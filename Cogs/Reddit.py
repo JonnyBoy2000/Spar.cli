70e0a1cafcb56bbcc526278cf8eb5c4ee21682d0
Callum Bartlett <coolum444@hotmail.co.uk>
2017-01-05 19:44:52 +0000

    :sparkles: Added subreddit command
    The subreddit command will give a random post from any given subreddit's
    hot 100 posts.

--- Cogs/Reddit.py
+++ Cogs/Reddit.py
@@ -8,6 +8,7 @@ from sys import path
 path.append('../')  # Move path so you can get the Utils folder
 from Utils.Discord import makeEmbed
 from Utils.Configs import getTokens
+from random import choice
 
 
 
@@ -23,11 +24,15 @@ class Reddit:
 
         self.getRedditor = lambda name: self.reddit.redditor(name)
         self.getSubreddit = lambda name: self.reddit.subreddit(name)
+        self.redditIcon = 'http://rawapk.com/wp-content/uploads/2016/04/Reddit-The-Official-App-Icon.png'
 
-    @commands.command()
-    async def reddituser(self, username:str):
+    @commands.command(pass_context=True)
+    async def reddituser(self, ctx, username:str):
         '''Gives info on a given redditor
         Usage :: reddituser <Username>'''
+
+        # Send typing to show that you're doing stuff
+        await self.sparcli.send_typing(ctx.message.server)
 
         # Get the redditor from using the reddit instance
         redditor = self.getRedditor(username)
@@ -52,11 +57,56 @@ class Reddit:
         redditData['Top Post'] = '[Click here!](http://reddit.com{0.permalink})'.format(topLink)
 
         # Make an embed from it
-        e = makeEmbed(name=redditor.name, colour=0xFFFFFF, values=redditData)
+        e = makeEmbed(name=redditor.name, icon=self.redditIcon, colour=0xff4006, values=redditData)
 
         # Print to user
         await self.sparcli.say('', embed=e)
 
+    @commands.command(pass_context=True, aliases=['reddit'])
+    async def subreddit(self, ctx, name:str):
+        '''Gets a random post from a subreddit
+        Usage :: subreddit <Subreddit>'''
+
+        # Send typing to show that you're doing stuff
+        await self.sparcli.send_typing(ctx.message.server)
+
+        # Get the subreddit from the reddit instance
+        subreddit = self.getSubreddit(name)
+
+        # Get a random post
+        postGen = subreddit.hot()
+        postList = [i for i in postGen]
+        post = choice(postList)
+        postValues = OrderedDict()
+
+        # Format nicely for the embeds
+        title = post.title
+        link = post.shortlink
+        score = post.score
+
+        # Start formatting the embed
+        postValues['Title'] = title 
+        postValues['Score'] = score
+        postValues['Comments'] = '[Click here!]({})'.format(link)
+
+        # Work out if it's an image or not
+        if post.is_self:
+            makeThumb = False
+            if post.selftext != '':
+                postValues['Body Text'] = post.selftext
+        elif True in [post.url.endswith(i) for i in['.jpg', '.jpeg', '.png']]:
+            makeThumb = True
+        else:
+            makeThumb = True
+
+        # Make the embed
+        if makeThumb:
+            e = makeEmbed(name=title, icon=self.redditIcon, colour=0xff4006, image=post.url, values=postValues)
+        else:
+            e = makeEmbed(name=title, icon=self.redditIcon, colour=0xff4006, values=postValues)
+
+        # Return to user
+        await self.sparcli.say('', embed=e)
 
 
 def setup(bot):
