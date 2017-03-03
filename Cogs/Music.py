a979630d7f5055b14f8b87643d06988f1a47ab01
Callum Bartlett <coolum444@hotmail.co.uk>
2017-03-03 22:58:00 +0000

    :package: Redone music handling and included queueing
    I want to say it's nicer but it really isn't

--- Cogs/Music.py
+++ Cogs/Music.py
@@ -1,10 +1,8 @@
 from discord.ext import commands 
 from discord import opus, ClientException
-from time import time as currentTime
-from datetime import timedelta
 from ctypes.util import find_library
-from collections import OrderedDict
 from Cogs.Utils.Discord import makeEmbed
+from Cogs.Utils.VoiceHandler import ServerVoice
 
 
 class Music:
@@ -12,7 +10,6 @@ class Music:
     def __init__(self, sparcli):
         self.sparcli = sparcli
         self.voice = {}
-        self.allowed = lambda x: self.voice[x]['LastCalled'] > currentTime() - 5
 
         # Start OPUS if not loaded
         if not opus.is_loaded():
@@ -22,221 +19,96 @@ class Music:
         for i in self.sparcli.servers:
 
             # Set up a nice dictionary for storage of information
-            self.voice[i] = {
-                'VoiceClient': self.sparcli.voice_client_in(i),
-                'StreamClient': None,
-                'Volume': 0.2,
-                'LastCalled': 0
-            }
+            voiceClientInServer = self.sparcli.voice_client_in(i)
+            self.voice[i] = ServerVoice(bot=self.sparcli, server=i, voiceClient=voiceClientInServer)
 
 
-    async def joinVC(self, *, member=None, channel=None):
-        '''Joins a given voice channel.
-        Will join the VC of a member if a member is passed'''
+    @commands.command(pass_context=True, aliases=['p'])
+    async def play(self, ctx, *, nameOfSong:str):
+        '''
+        Plays a song from a YouTube search or URL.
+        '''
 
-        # Join a channel if specified
-        if channel != None:
-            loadedVC = self.sparcli.join_voice_channel(channel)
-            return loadedVC
-
-        # No channel specified, join the user's VC
-        memberVC = member.voice.voice_channel
-        if memberVC == None:
-            await self.sparcli.say('You aren\'t currently in a voice channel.')
-            return False
-
-        # Join the member's VC
-        try:
-            # Try to join
-            loadedVC = await self.sparcli.join_voice_channel(memberVC)
-            return loadedVC
-
-        except ClientException:
-            # You're already in it
-            loadedVC = self.sparcli.voice_client_in(member.server)
-            return loadedVC
-
-        else:
-            # You can't join it
-            await self.sparcli.say('I am unable to join that voice channel.')
-            return False
-
-    async def youtubeBridge(self, server, songName):
-        '''Plays a song through a voice client via YTDL'''
-
-        # Make sure it filters through ytdl properly
-        if 'http://' in songName.lower() or 'https://' in songName.lower():
-            searchTerm = songName
-        else:
-            searchTerm = 'ytsearch:' + songName
-
-        voiceClient = self.voice[server]['VoiceClient']
-        
-        # Create the streamclient
-        try:
-            streamClient = await voiceClient.create_ytdl_player(searchTerm)
-        except Exception as e:
-           
-           # Could not create the stream client
-           await self.sparcli.say('I was unable to create the voice client against this search term.')
-           await self.sparcli.say(e + str(e))
-           return
-
-        # Check the blacklist for the thinamawhatsit
-        if not self.handleBlacklist(streamClient):
-            await self.sparcli.say('Your search term\'s first result returns a video with a blacklisted title.')
-            return
-
-        # Stop the old streamclient
-        oldStreamClient = self.voice[server]['StreamClient']
-        try:
-            oldStreamClient.stop()
-        except:
-            # It was probably nonetype
-            pass
-
-        # Start the new one
-        streamClient.volume = self.voice[server]['Volume']
-        streamClient.start()
-        self.voice[server]['LastCalled'] = currentTime()
-        self.voice[server]['StreamClient'] = streamClient
-
-        # Print out to user
-        # duration = timedelta(seconds=streamClient.duration)
-        # title = streamClient.title
-        # await self.sparcli.say('Now playing `{0}` :: `[{1}]`'.format(title, duration))
-        await self.playEmbed(streamClient)
-
-    async def playEmbed(self, streamClient):
-        '''Creates and sends an embed based on the steamclient'''
-
-        title = streamClient.title
-        duration = timedelta(seconds=streamClient.duration)
-        description = streamClient.description
-        likes = streamClient.likes
-        dislikes = streamClient.dislikes
-        views = streamClient.views
-        uploader = streamClient.uploader
-        date = streamClient.upload_date
-
-        if len(description) > 100:
-            fdesc = description[:100] + '...'
-        else:
-            fdesc = description
-
-        o = OrderedDict()
-        o['Title'] = (title, False)
-        o['Views'] = views 
-        o['Duration'] = duration 
-        o['Uploader'] = uploader 
-        o['Upload Date'] = date
-        o['Likes'] = likes 
-        o['Dislikes'] = dislikes 
-        o['Description'] = (fdesc, False)
-
-        e = makeEmbed(name='Now Playing!', values=o, user=self.sparcli.user)
-        await self.sparcli.say('', embed=e)
-
-    def handleBlacklist(self, streamClient):
-        '''Sets a blacklist for certain terms - earrape, etc'''
-
-        # Get the song title
-        title = streamClient.title
-
-        # Sets up the blacklist
-        blacklistedTerms = [
-            'ear rape',
-            'earrape',
-            'rip headphone',
-            'ripheadphone',
-            'thomas the pain'
-        ]
-
-        # Make check if it can filter through
-        if title.lower() in blacklistedTerms:
-            return False
-
-        # It *should* be fine I think
-        return True
+        serverHandler = self.voice[ctx.message.server]
+        serverHandler.lastChannel = ctx.message.channel
+        if serverHandler.voiceClient == None:
+            z = await serverHandler.joinVC(ctx.message.author)
+            if z == False:
+                return
+        await serverHandler.addToQueue(nameOfSong)
+        if serverHandler.looping == False: await serverHandler.loop()
 
     @commands.command(pass_context=True)
     async def bestsong(self, ctx):
         '''Plays the best song.'''
 
-        nameOfSong='https://www.youtube.com/watch?v=miomuSGoPzI'
-        member = ctx.message.author
-        server = member.server
+        nameOfSong = 'https://www.youtube.com/watch?v=miomuSGoPzI'
 
-        # Join voice channel
-        voiceClient = await self.joinVC(member=member)
-        if voiceClient == False: return
-        self.voice[server]['VoiceClient'] = voiceClient
-
-        # Go play the stuff
-        await self.youtubeBridge(server, nameOfSong)
-
-    @commands.command(pass_context=True, aliases=['p'])
-    async def play(self, ctx, *, nameOfSong:str):
-        '''Gets a song from YouTube and plays it through the bot
-        Usage :: play <SongName>
-              :: play <YouTube Link>
-              :: play <SoundCloud Link>'''
-
-        member = ctx.message.author
-        server = member.server
-
-        # Join voice channel
-        voiceClient = await self.joinVC(member=member)
-        if voiceClient == False: return
-        self.voice[server]['VoiceClient'] = voiceClient
-
-        # Go play the stuff
-        await self.youtubeBridge(server, nameOfSong)
+        serverHandler = self.voice[ctx.message.server]
+        serverHandler.lastChannel = ctx.message.channel
+        if serverHandler.voiceClient == None:
+            z = await serverHandler.joinVC(ctx.message.author)
+            if z == False:
+                return
+        await serverHandler.addToQueue(nameOfSong)
+        if serverHandler.looping == False: await serverHandler.loop()
 
     @commands.command(pass_context=True, aliases=['disconnect', 'dc'])
     async def leave(self, ctx):
-        '''Makes a bot on the server leave the joined voice channel
-        Usage :: leave'''
+        '''
+        Makes a bot on the server leave the joined voice channel.
+        '''
 
-        server = ctx.message.server
-
-        voiceClient = self.voice[server]['VoiceClient']
-        streamClient = self.voice[server]['StreamClient']
-
-        # Stop any playing media
-        try:
-            streamClient.stop()
-        except AttributeError:
-            pass
-
-        # Leave the voice channel
-        try:
-            await voiceClient.disconnect()
-        except AttributeError:
+        serverHandler = self.voice[ctx.message.server]
+        serverHandler.lastChannel = ctx.message.channel
+        if serverHandler.voiceClient == None:
             await self.sparcli.say('I\'m not currently in a voice channel.')
             return
 
+        await serverHandler.disconnect()
         await self.sparcli.say('Disconnected from the VC.')
-        self.voice[server]['VoiceClient'] = None
-        self.voice[server]['StreamClient'] = None
+        if serverHandler.looping == False: await serverHandler.loop()
 
     @commands.command(pass_context=True, aliases=['v'])
     async def volume(self, ctx, volume:int=20):
-        '''Changes the volume of the currently playing music stream
-        Usage :: volume 63'''
+        '''
+        Changes the volume of the currently playing music stream.
+        '''
 
-        server = ctx.message.server
+        serverHandler = self.voice[ctx.message.server]
+        serverHandler.lastChannel = ctx.message.channel
 
-        streamClient = self.voice[server]['StreamClient']
-        if volume < 0:
-            volume = 0
-        elif volume > 100:
-            volume = 100
+        z = serverHandler.setVolume(volume)
 
-        streamClient.volume = volume / 100
-        self.voice[server]['Volume'] = volume / 100
+        await self.sparcli.say('The volume has been set to {}%.'.format(z))
+        if serverHandler.looping == False: await serverHandler.loop()
 
-        await self.sparcli.say('The volume has been set to {}%.'.format(volume))
+    @commands.command(pass_context=True)
+    async def queued(self, ctx):
+        '''
+        Gets you a list of what is currently queued.
+        '''
+
+        serverHandler = self.voice[ctx.message.server]
+        serverHandler.lastChannel = ctx.message.channel
+
+        queuedTitles = [i.title for i in serverHandler.queue]
+        if len(queuedTitles) > 0:
+            out = 'Here is a list of the currently queued items: ```\n{}```'.format('\n'.join(queuedTitles))
+        else:
+            out = 'There is currently nothing queued to be played :c'
+        await self.sparcli.say(out)
+
+        if serverHandler.looping == False: await serverHandler.loop()
+
+
+    # async def EXAMPLETHINGY(self):
+    #     serverHandler = self.voice[ctx.message.server]
+    #     serverHandler.lastChannel = ctx.message.channel
+
+    #     # CODE GOES HERE
+
+    #     if serverHandler.looping == False: await serverHandler.loop()
 
 
 def setup(bot):
---
+++ Cogs/Utils/VoiceHandler.py
@@ -0,0 +1,266 @@
+from discord import Channel
+from string import punctuation as AllPunctuation
+from asyncio import sleep
+from datetime import timedelta
+from collections import OrderedDict
+
+
+class ServerVoice(object):
+    '''
+    A handler for the voice client on a server
+    '''
+
+    working = False
+
+    def __init__(self, bot, **kwargs):
+        self.sparcli = bot
+        self.lastChannel = kwargs.get('lastChannel', kwargs['server'])
+        self.queue = kwargs.get('queue', [])
+        self.skipCount = kwargs.get('skipCount', 0)
+        self.voiceClient = kwargs.get('voiceClient', None)
+        self.streamClient = kwargs.get('streamClient', None)
+        self.volume = kwargs.get('volume', 0.2)
+        self.looping = False
+
+    async def joinVC(self, toJoin, channel=None):
+        '''
+        Joins a given voice channel.
+        Will join the VC of a member if a member is passed.
+        '''
+
+        if channel == None:
+            channel = self.lastChannel
+
+        # Join a channel if specified
+        if type(toJoin) == Channel:
+            loadedVC = self.sparcli.join_voice_channel(toJoin)
+            self.voiceClient = loadedVC
+            return loadedVC
+
+        # No channel specified, join the user's VC
+        memberVC = toJoin.voice.voice_channel
+        if memberVC == None:
+            await self.sparcli.send_message(channel, 'You aren\'t currently in a voice channel.')
+            return False
+
+        # Join the member's VC
+        try:
+            # Try to join
+            loadedVC = await self.sparcli.join_voice_channel(memberVC)
+            self.voiceClient = loadedVC
+            return loadedVC
+
+        except ClientException:
+            # You're already in it
+            loadedVC = self.sparcli.voice_client_in(toJoin.server)
+            self.voiceClient = loadedVC
+            return loadedVC
+
+        else:
+            # You can't join it
+            await self.sparcli.send_message(channel, 'I am unable to join that voice channel.')
+            return False
+
+    def handleBlacklist(self, streamClient):
+        '''
+        Sets a blacklist for certain terms - earrape, etc.
+        '''
+
+        # Get the song title
+        title = streamClient.title
+
+        # Sets up the blacklist
+        blacklistedTerms = [
+            'ear rape',
+            'earrape',
+            'rip headphone',
+            'ripheadphone',
+            'thomas the pain'
+        ]
+
+        # Make check if it can filter through
+        if True in [i.lower() in title.lower() for i in blacklistedTerms]:
+            return False
+
+        # It *should* be fine I think
+        return True
+
+    async def createPlayer(self, songName, channel=None):
+        '''
+        Creates a YTDL player for a given searchterm.
+        '''
+
+        if channel == None:
+            channel = self.lastChannel
+
+        # Make sure it filters through ytdl properly
+        if 'http://' in songName.lower() or 'https://' in songName.lower():
+            searchTerm = songName
+        else:
+            searchTerm = 'ytsearch:' + songName
+
+        voiceClient = self.voiceClient
+        
+        # Create the streamclient
+        try:
+            while ServerVoice.working:
+                await sleep(0.1)
+            ServerVoice.working = True
+            streamClient = await voiceClient.create_ytdl_player(searchTerm)
+            ServerVoice.working = False
+
+        except Exception as e:           
+           # Could not create the stream client
+           await self.sparcli.send_message(channel, 'I was unable to find an applicable YouTube video for the search term `{}`.'.format(songName))
+           await self.sparcli.send_message(channel, e + str(e))
+           return None
+
+        # Check the blacklist for the thinamawhatsit
+        if self.handleBlacklist(streamClient) == False:
+            await self.sparcli.send_message(channel, 'Your search term\'s first result returns a video with a blacklisted title (`{.title}`).'.format(streamClient))
+            return None
+
+        return streamClient
+
+    async def startPlayer(self, player):
+        '''
+        Starts a player and sets it to the streamClient attribute.
+        '''
+
+        # Start the new one
+        try: self.streamClient.stop()
+        except AttributeError: pass
+        player.volume = self.volume
+        player.start()
+        self.skipCount = 0
+        self.streamClient = player
+
+        # Print out to user
+        await self.playEmbed(player)
+
+    async def playEmbed(self, player, channel=None):
+        '''
+        Creates and sends an embed based on the steamclient.
+        '''
+
+        if channel == None:
+            channel = self.lastChannel
+
+        title = player.title
+        duration = timedelta(seconds=player.duration)
+        description = player.description
+        likes = player.likes
+        dislikes = player.dislikes
+        views = player.views
+        uploader = player.uploader
+        date = player.upload_date
+
+        if len(description) > 100:
+            d = description.split(' ')
+            f = []
+            while len(' '.join(f)) < 100:
+                f.append(d[0])
+                del d[0]
+            g = ' '.join(f)
+            fdesc = g if g == description else g + '...'
+            if fdesc[-3:] == '...':
+                if fdesc[-4] in AllPunctuation: 
+                    fdesc = fdesc[:-4] + '...'
+        else:
+            fdesc = description
+
+        o = OrderedDict()
+        o['Title'] = (title, False)
+        o['Views'] = views 
+        o['Duration'] = duration 
+        o['Uploader'] = uploader 
+        o['Upload Date'] = date
+        o['Likes'] = likes 
+        o['Dislikes'] = dislikes 
+        o['Description'] = (fdesc, False)
+
+        e = makeEmbed(name='Now Playing!', values=o, user=self.sparcli.user)
+        await self.sparcli.send_message(channel, '', embed=e)
+
+    async def addToQueue(self, whatToAdd, channel=None):
+        '''
+        Adds an item to the queue of the VC.
+        '''
+
+        if channel == None:
+            channel = self.lastChannel
+
+        q = await self.createPlayer(whatToAdd)
+        if q == None: return
+        self.queue.append(q)
+        await self.sparcli.send_message(channel, 'The video `{.title}` has been added to the queue.'.format(q))
+
+    async def disconnect(self):
+        '''
+        Disconnects the voice client from any voice channel.
+        '''
+
+        try:
+            self.streamClient.stop()
+        except AttributeError:
+            pass
+
+        # Leave the voice channel
+        await self.voiceClient.disconnect()
+
+        self.queue = []
+        self.voiceClient = None
+        self.streamClient = None
+
+    def setVolume(self, amount:int):
+        '''
+        Changes the volume level.
+        '''
+
+        if amount > 100: amount = 100
+        if amount < 0: amount = 0
+        percentage = amount/100.0
+
+        try:
+            self.streamClient.volume = percentage
+        except AttributeError:
+            pass
+
+        self.volume = percentage
+        return amount
+
+    async def loop(self):
+        '''
+        Defines the looping segment that constantly checks the queue for songs to play
+        '''
+
+        doPlayNext = True
+        self.looping = True
+
+        while True:
+
+            # Check if the queue is empty
+            if len(self.queue) > 0:
+
+                # It's not, so check if playing anything currently
+                try:
+
+                    # It isn't playing
+                    if self.streamClient.is_done():
+                        doPlayNext = True
+
+                    # It is playing
+                    else:
+                        doPlayNext = False
+
+                # No current client
+                except Exception:
+                    doPlayNext = True 
+
+            # Play the next thing in the queue
+            if doPlayNext and len(self.queue) > 0:
+                await self.startPlayer(self.queue[0])
+                del self.queue[0]
+
+            # Now sleep
+            await sleep(3)
