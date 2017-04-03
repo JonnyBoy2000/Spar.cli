26622bc87f332275d4a438ceae3e2620ef176e1c
Callum Bartlett <coolum444@hotmail.co.uk>
2017-04-04 00:41:28 +0100

    :sparkles: Added level parsing
    I added actual experience gain a while ago, but I hadn't added any kind
    of way to see it until now.
    The current level experience relation is L = E/375, since I wanted
    15000XP to be levl 40, but I may have to change that in future depending
    on how much traction it has, and how quickly people progress levels. An
    exponential function is in mind, but I don't know how to factor it
    properly just yet.

--- Cogs/Levels.py
+++ Cogs/Levels.py
@@ -1,5 +1,8 @@
+from math import log
+from discord import Member
 from discord.ext import commands 
 from Cogs.Utils.LevelHandler import LevelHandler
+from Cogs.Utils.Configs import getMoneyJson
 
 
 class Levels:
@@ -7,6 +10,59 @@ class Levels:
     def __init__(self, sparcli):
         self.sparcli = sparcli
         self.levelHandler = LevelHandler()
+
+    @commands.command(pass_context=True)
+    async def xp(self, ctx, user:Member=None):
+        '''
+        Shows you how much experience you have.
+        '''
+
+        if user == None:
+            user = ctx.message.author
+
+        serverXP = getMoneyJson(str(ctx.message.server.id))
+        userXP = serverXP.get(str(user.id), 0)
+
+        await self.sparcli.say('**{}** has **{}**XP on this server.'.format(user.display_name. userXP))
+
+    @commands.command(pass_context=True)
+    async def level(self, ctx, user:Member=None):
+        '''
+        Shows you how much experience you have.
+        '''
+
+        if user == None:
+            user = ctx.message.author
+
+        serverXP = getMoneyJson(str(ctx.message.server.id))
+        userXP = serverXP.get(str(user.id), 0)
+        userLevel = userXP/375
+
+        await self.sparcli.say('**{}** is level **{:.2f}** on this server.'.format(user.display_name, userLevel))
+
+    @commands.command(pass_context=True)
+    async def leaderboard(self, ctx):
+
+        leaderboard = {i: o for o, i in getMoneyJson(str(ctx.message.server.id)).items()}
+        xpStains = [i for i in leaderboard.keys()]
+        xpStains = sorted(xpStains, reverse=True)
+
+        lads = []
+        for i in range(10):
+            z = xpStains[i]
+            lads.append([leaderboard[z], z])
+
+        ladz = []
+        for i in lads:
+            if ctx.message.server.get_member(i[0]):
+                ladz.append([ctx.message.server.get_member(i[0]), i[1]])
+
+        ret = ''
+        for i in ladz:
+            ret = ret + i[0].display_name + ' - **Lvl {:.2f}** \n'.format(i[1]/375)
+
+        await self.sparcli.say(ret)
+
 
     async def on_message(self, message):
         '''
